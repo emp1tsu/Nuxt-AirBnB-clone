4d59703247a061b2290030a2c6c88dd05c1fcdd6
tanakakjs <oneok0813@gmail.com>
2020-12-20 22:49:10 +0900

    Changes

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,14 +1,72 @@
 <template>
-  <div>{{ home.title }}</div>
+  <div>
+    <div style="display:flex">
+      <img
+        v-for="image in home.images"
+        :key="image"
+        alt="alt..."
+        :src="image"
+        width="200"
+        height="150"
+      />
+    </div>
+    {{ home.title }}
+    ${{ home.pricePerNight }} / night<br />
+    <img src="/images/marker.svg" width="20" height="20" />{{
+      home.location.address
+    }}
+    <img src="/images/star.svg" width="20" height="20" />{{
+      home.location.city
+    }}
+    {{ home.location.state }} {{ home.location.country }}<br />
+    {{ home.reviewValue }}<br />
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
+    {{ home.bathrooms }} bath<br />
+    {{ home.description }}
+    <div style="height:800px;width:800px" ref="map"></div>
+  </div>
 </template>
 
 <script>
 import homes from "~/data/homes";
 export default {
+  head() {
+    return {
+      title: this.home.title,
+      script: [
+        {
+          src:
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyDPpI4HRbEdTahtsvMJd-VUJKYDSkK4q80&libraries=places",
+          hid: "map",
+          defer: true,
+        },
+      ],
+    };
+  },
   data() {
     return {
       home: {},
     };
+  },
+  mounted() {
+    const mapOptions = {
+      zoom: 18,
+      center: new window.google.maps.LatLng(
+        this.home._geoloc.lat,
+        this.home._geoloc.lng
+      ),
+      disableDefaultUI: true,
+      zoomControll: true,
+    };
+    const map = new window.google.maps.Map(this.$refs.map, mapOptions);
+    const position = new window.google.maps.LatLng(
+      this.home._geoloc.lat,
+      this.home._geoloc.lng
+    );
+    const marker = new window.google.maps.Marker({
+      position,
+    });
+    marker.setMap(map);
   },
   created() {
     const home = homes.find((home) => home.objectID === this.$route.params.id);
